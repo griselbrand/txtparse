077064dc6398ca2b6f03b05400752bef54145ece
Griselbrand <griselbrand@gmail.com>
2015-10-17 22:09:11 +0900

    add attributes of header

--- Entry.pm
+++ Entry.pm
@@ -1,4 +1,43 @@
-package Entry;
+package Header;
+
 use Moose;
 
-has 'header' => ( is '
+has 'path_num' => (
+
+	is => 'rw',
+	isa => 'Str',
+
+);
+
+
+has 'pt_id' => (
+
+	is => 'rw',
+	isa => 'Str',
+
+);
+
+
+has 'sp_name_rp' => (
+
+	is => 'rw',
+	isa => 'Str',
+
+);
+
+
+has 'sp_name_sp' => (
+
+	is => 'rw',
+	isa => 'Str',
+
+);
+
+has 'orient' => (
+
+	is => 'rw',
+	isa => 'Str',
+
+);
+
+
--- txtparse.pl
+++ txtparse.pl
@@ -20,8 +20,8 @@ foreach my $string ( @list ) {
 my $result = join (" ", @list);
 
 
- if ( $result =~ /\*병리번호: (?<path_num>S15.*?)\s.*?\*등록번호: (?<pt_id>\d+?)\s.*?\*대표검체: (?<name_sp>\w+?)\s.*?\*세부검체: (?<name_sp2>\w+),\s?(?<orient>\w+)?\s/) {
+ if ( $result =~ /\*병리번호: (?<path_num>S15.*?)\s.*?\*등록번호: (?<pt_id>\d+?)\s.*?\*대표검체: (?<sp_name_rp>\w+?)\s.*?\*세부검체: (?<sp_name_sp>\w+),\s?(?<orient>\w+)?\s/) {
 
-	$+{path_num}, $+{pt_id}, $+{name_sp}, $+{name_sp2}, $+{orient}
+	$+{path_num}, $+{pt_id}, $+{sp_name_rp}, $+{sp_name_sp}, $+{orient}
 }
 
